80383b5a8a6bdaa152d669f008cbcafab0e01119
yuanzhe <1035877660@qq.com>
2025-06-10 10:40:18 +0800

    添加目录

--- src/pages/index/Index.vue
+++ src/pages/index/Index.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+</script>
 
 <template>
 	<div class="index">
-		<svg class="icon"><use href="#angle-down"></use></svg>
+		<button @click="router.push('/editor')">编辑器</button>
 	</div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.index {
+	.icon {
+		color: #fff;
+	}
+}
+</style>
