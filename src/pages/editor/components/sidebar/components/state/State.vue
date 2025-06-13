3246b7cf0c9c5bd143987df486c17e71ca462246
yuanzhe <1035877660@qq.com>
2025-06-14 04:24:25 +0800

    完善变量

--- src/pages/editor/components/sidebar/components/state/State.vue
+++ src/pages/editor/components/sidebar/components/state/State.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
-import { useAsset } from "@/stores/useAsset";
-import { useDragger } from "@/pages/editor/hooks/useDragger";
+import { ref } from "vue";
+import { useSchema } from "@/stores/useSchema";
+import { deserialize, serialize } from "@/utils/conversion";
 
-const assetStore = useAsset();
-const dragger = useDragger();
+const schemaStore = useSchema();
+const value = ref("");
+init();
+function init() {
+	value.value = serialize(schemaStore.state);
+}
+const input = () => {
+	const state = deserialize(value.value);
+	if (state) schemaStore.state = state;
+};
 </script>
 
 <template>
 	<div class="state">
-		<textarea></textarea>
+		<textarea v-model="value" @input="input()"></textarea>
 	</div>
 </template>
 
@@ -25,6 +34,7 @@ const dragger = useDragger();
 		padding: 10px;
 		background-color: #232323;
 		border-radius: 10px;
+		resize: none;
 	}
 }
 </style>
--- src/utils/conversion.ts
+++ src/utils/conversion.ts
@@ -1,25 +1,41 @@
 // 深拷贝
 export const deepClone = <T>(object: T, hash = new WeakMap()): T => {
-    if (object instanceof Date) return new Date((object as Date).getTime()) as unknown as T;
-    if (object instanceof RegExp) return new RegExp(object) as unknown as T;
-    if (typeof object !== "object" || object === null) return object;
-    if (hash.has(object)) return hash.get(object);
-    let cloneObj: any;
-    if (Array.isArray(object)) cloneObj = [];
-    else if (object instanceof Map) cloneObj = new Map();
-    else if (object instanceof Set) cloneObj = new Set();
-    else cloneObj = {};
-    hash.set(object, cloneObj);
-    for (const key in object) {
-        if (object.hasOwnProperty(key)) {
-            if (object instanceof Map) cloneObj.set(key, deepClone(object.get(key), hash));
-            else if (object instanceof Set) cloneObj.add(deepClone(key, hash));
-            else cloneObj[key] = deepClone(object[key], hash);
-        }
-    }
-    return cloneObj as T;
+	if (object instanceof Date) return new Date((object as Date).getTime()) as unknown as T;
+	if (object instanceof RegExp) return new RegExp(object) as unknown as T;
+	if (typeof object !== "object" || object === null) return object;
+	if (hash.has(object)) return hash.get(object);
+	let cloneObj: any;
+	if (Array.isArray(object)) cloneObj = [];
+	else if (object instanceof Map) cloneObj = new Map();
+	else if (object instanceof Set) cloneObj = new Set();
+	else cloneObj = {};
+	hash.set(object, cloneObj);
+	for (const key in object) {
+		if (object.hasOwnProperty(key)) {
+			if (object instanceof Map) cloneObj.set(key, deepClone(object.get(key), hash));
+			else if (object instanceof Set) cloneObj.add(deepClone(key, hash));
+			else cloneObj[key] = deepClone(object[key], hash);
+		}
+	}
+	return cloneObj as T;
 };
 // 补零
 export const zeroAdd = (n: string | number) => {
-    return Number(n) < 10 ? "0" + n : n.toString();
+	return Number(n) < 10 ? "0" + n : n.toString();
+};
+// 序列化对象
+export const serialize = (obj: any): string => {
+	try {
+		return JSON.stringify(obj, null, 4);
+	} catch (error) {
+		return "";
+	}
+};
+// 反序列化字符串
+export const deserialize = (str: string): any => {
+	try {
+		return new Function("return " + str)();
+	} catch (error) {
+		return null;
+	}
 };
