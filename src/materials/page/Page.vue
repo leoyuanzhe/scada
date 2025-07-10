9b9e714bc8637f42522140b6b75b9861fb7abf71
yuanzhe4_v <yuanzhe4_v@didiglobal.com>
2025-07-10 21:10:06 +0800

    完善page组件重构

---
+++ src/materials/page/Page.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { Component } from "@/types/Component";
+import type { PageProps } from "./Page";
+import ComponentCreator from "@/components/component-creator/ComponentCreator.vue";
+
+interface Props {
+	component: Component<PageProps>;
+}
+const props = withDefaults(defineProps<Props>(), {});
+</script>
+
+<template>
+	<div class="page">
+		<ComponentCreator v-for="v in props.component.components" :key="v.id" :component="v" />
+	</div>
+</template>
+
+<style scoped lang="scss"></style>
