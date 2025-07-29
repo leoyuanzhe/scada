aad0577594cfcec1d3c4329c22c5a042d5761359
yuanzhe4_v <yuanzhe4_v@didiglobal.com>
2025-07-29 16:11:45 +0800

    优化设置器

--- src/pages/editor/components/setter/Setter.vue
+++ src/pages/editor/components/setter/Setter.vue
@@ -8,9 +8,31 @@ import StateConfig from "./components/StateConfig.vue";
 import DataSourceConfig from "./components/DataSourceConfig.vue";
 import EmitConfig from "./components/EmitConfig.vue";
 
+interface Props {
+	width: number;
+}
+interface Emits {
+	(e: "update:width", width: number): void;
+}
 const schemaStore = useSchema();
+const props = withDefaults(defineProps<Props>(), {});
+const emits = defineEmits<Emits>();
 const current = ref<"global" | "layout" | "prop" | "state" | "dataSource" | "emit">("layout");
 const targetComponentV2 = computed(() => schemaStore.targetComponent ?? schemaStore.currentRootComponent);
+const resizerOnMouseDown = (e: MouseEvent) => {
+	document.body.addEventListener("mousemove", onMouseMove);
+	document.body.addEventListener("mouseup", onMouseUp);
+	const startX = e.clientX;
+	const startWidth = props.width;
+	function onMouseMove(e: MouseEvent) {
+		const moveX = e.clientX - startX;
+		emits("update:width", startWidth - moveX);
+	}
+	function onMouseUp() {
+		document.body.removeEventListener("mousemove", onMouseMove);
+		document.body.removeEventListener("mouseup", onMouseUp);
+	}
+};
 </script>
 
 <template>
@@ -34,11 +56,13 @@ const targetComponentV2 = computed(() => schemaStore.targetComponent ?? schemaSt
 			<DataSourceConfig v-if="current === 'dataSource' && targetComponentV2" :component="targetComponentV2" />
 			<EmitConfig v-if="current === 'emit' && targetComponentV2" :component="targetComponentV2" />
 		</div>
+		<div class="resizer" @mousedown="resizerOnMouseDown($event)"></div>
 	</div>
 </template>
 
 <style scoped lang="scss">
 .setter {
+	position: relative;
 	display: flex;
 	flex-direction: column;
 	overflow: hidden;
@@ -49,7 +73,9 @@ const targetComponentV2 = computed(() => schemaStore.targetComponent ?? schemaSt
 		background-color: #333;
 		box-shadow: 0 0 3px 1px #666;
 		z-index: 1;
+		overflow: auto;
 		button {
+			flex-shrink: 0;
 			width: 40px;
 			height: 40px;
 			background-color: #444;
@@ -70,6 +96,21 @@ const targetComponentV2 = computed(() => schemaStore.targetComponent ?? schemaSt
 		flex-direction: column;
 		row-gap: 10px;
 		overflow: auto;
+	}
+	.resizer {
+		position: absolute;
+		left: 0;
+		top: 0;
+		width: 4px;
+		height: 100%;
+		background-color: var(--primary-color);
+		transition: opacity 0.2s;
+		cursor: col-resize;
+		z-index: 1;
+		opacity: 0;
+		&:hover {
+			opacity: 1;
+		}
 	}
 }
 </style>
